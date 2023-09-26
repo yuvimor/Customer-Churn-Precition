05d5611bc9dc02e4bcec6d1dcfb7fa39ed9eb1a5
Yuvraj Singh Mor <140009504+yuvimor@users.noreply.github.com>
2023-09-26 23:04:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 import streamlit as st
 import joblib
 import pandas as pd
@@ -7,7 +6,7 @@ from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 # Load the saved XGBoost classifier model
 loaded_model = joblib.load('customer_churn_classifier.pkl')
 
-# Load the training data from a CSV file 
+# Load the training data from a CSV file 
 X_train = pd.read_csv('train_data.csv')
 
 # Initialize LabelEncoder for Location
@@ -37,33 +36,35 @@ location = st.selectbox("Location", ["Houston", "Los Angeles", "Miami","Chicago"
 
 # Define a function to make predictions
 def predict_churn(churn_history_count, monthly_bill, billing_to_usage_ratio, usage_per_billing_cycle, total_usage_gb, age, tenure_years, location):
-    # Prepare input data as a DataFrame
-    input_data = pd.DataFrame({
-        'Churn_History_Count': [churn_history_count],
-        'Monthly_Bill': [monthly_bill],
-        'Billing_to_Usage_Ratio': [billing_to_usage_ratio],
-        'Usage_Per_Billing_Cycle': [usage_per_billing_cycle],
-        'Total_Usage_GB': [total_usage_gb],
-        'Age': [age],
-        'Tenure_Years': [tenure_years],
-        'Location': [location]
-    })
+  # Prepare input data as a DataFrame
+  input_data = pd.DataFrame({
+    'Churn_History_Count': [churn_history_count],
+    'Monthly_Bill': [monthly_bill],
+    'Billing_to_Usage_Ratio': [billing_to_usage_ratio],
+    'Usage_Per_Billing_Cycle': [usage_per_billing_cycle],
+    'Total_Usage_GB': [total_usage_gb],
+    'Age': [age],
+    'Tenure_Years': [tenure_years],
+    'Location': [location]
+  })
 
-    # Label encode the 'Location' column
-    input_data['Location'] = label_encoder.transform(input_data['Location'])
+  # Label encode the 'Location' column
+  input_data['Location'] = label_encoder.transform(input_data['Location'])
 
-    # Scale the numeric input data using Min-Max scaling
-    input_data[numeric_columns] = scaler.transform(input_data[numeric_columns])
+  # Scale the numeric input data using Min-Max scaling
+  input_data[numeric_columns] = scaler.transform(input_data[numeric_columns])
 
-    # Use the loaded model to make predictions
-    prediction = loaded_model.predict(input_data)
+  # Use the loaded model to make predictions
+  prediction = loaded_model.predict(input_data)
 
-    return prediction  # Return the prediction result
+  return prediction
 
 # Get predictions when the user clicks a button
 if st.button("Predict"):
-    result = predict_churn(churn_history_count, monthly_bill, billing_to_usage_ratio, usage_per_billing_cycle, total_usage_gb, age, tenure_years, location)
-    if result == 1:
-        st.write("Churn Prediction: Churn")
-    else:
-        st.write("Churn Prediction: No Churn")
+  result = predict_churn(churn_history_count, monthly_bill, billing_to_usage_ratio, usage_per_billing_cycle, total_usage_gb, age, tenure_years, location)
+
+  # Display the prediction result
+  if result == 1:
+    st.write("Churn Prediction: Churn")
+  else:
+    st.write("Churn Prediction: No Churn")
